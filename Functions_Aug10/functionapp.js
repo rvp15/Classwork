e24c3b7f87bfddaea328c969f78212f3a6b85dcf
rvp15 <vedhagithub@gmail.com>
2022-08-11 12:31:43 -0400

    Function ClassLab

---
+++ Functions_Aug10/functionapp.js
@@ -0,0 +1,44 @@
+// Topic : Function
+
+// Exercise1:Write a function declaration
+
+function computeArea(width, height) {
+  let area = width * height;
+  console.log(
+    `The area of a rectangle with a width is ${width} and a height of ${height} is ${area} square units`
+  );
+}
+computeArea(2, 5);
+
+// Exercise 2:
+
+const planetHasWater = function (a) {
+  let caseSen = a.toLowerCase();
+  if (caseSen === "earth" || caseSen === "mars") {
+    return "true";
+  } else {
+    return "false";
+  }
+};
+console.log(planetHasWater("erth"));
+
+// Exercise 3:Arrow function
+
+const computeArea1 = (width, height) => {
+  let area = width * height;
+  console.log(
+    `The area of a rectangle with a width is ${width} and a height of ${height} is ${area} square units`
+  );
+};
+computeArea1(5, 6);
+
+// #2:
+const planetHasWater1 = (a) => {
+  let caseSen = a.toLowerCase();
+  if (caseSen === "earth" || caseSen === "mars") {
+    return "true";
+  } else {
+    return "false";
+  }
+};
+console.log(planetHasWater1("Mars"));
