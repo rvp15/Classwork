a5bada0fd1c1ef80e2a024e5c739ed93c9b8a75b
rvp15 <vedhagithub@gmail.com>
2022-08-29 08:09:32 -0400

    FinishedEventLab

--- DOM_Practice_Aug24/DOM_ClassLab/script.js
+++ DOM_Practice_Aug24/DOM_ClassLab/script.js
@@ -8,25 +8,25 @@ let mainEl = document.querySelector("main");
 
 mainEl.style.backgroundColor = "var(--main-bg)";
 
-//Task 1.2
+// //Task 1.2
 mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
 
-//Task 1.3
+// //Task 1.3
 mainEl.setAttribute("class", "flex-ctr");
 
-//Task 2.0
+// //Task 2.0
 
 let topMenuEl = document.getElementById("top-menu");
-
-//Task 2.1
+// console.log(topMenuEl)
+// //Task 2.1
 
 topMenuEl.style.height = "100%";
 
-//Task 2.2
+// //Task 2.2
 
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
 
-//Task 2.3
+// //Task 2.3
 
 topMenuEl.setAttribute("class", "flex-around");
 
@@ -51,7 +51,7 @@ for (let link of menuLinks) {
 //Task 4.0
 const subMenuEl = document.querySelector("#sub-menu");
 
-//Task 4..1
+//Task 4.1
 subMenuEl.style.height = "100%";
 
 //Task 4.2
@@ -97,44 +97,96 @@ var menuLinks = [
     ],
   },
 ];
-//Task 5.1
+// //Task 5.1
+// topMenuLinks= document.querySelectorAll(".flex-around a")
+// console.log(topMenuLinks)
+// or
 topMenuLinks = topMenuEl.querySelectorAll("a");
-console.log(topMenuLinks);
+// console.log(topMenuLinks);
 var showingSubMenu = false;
 
-//Task 5.2
+// //Task 5.2
 topMenuEl.addEventListener("click", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // this will stop  default events
   if (event.target.tagName !== "A") {
     return;
   }
-  console.log(event.target.text);
-  //Task 5.3
-  console.log(event.target.className);
-  if (event.target.className === "active") {
+  // console.log(event.target.text);
+  let clickedItem;
+  for (const menuItem of menuLinks) {
+    if (menuItem.text == event.target.text) {
+      clickedItem = menuItem;
+      break;
+    }
+  }
+
+  //Task 5.3:
+  // console.log(event.target.classList)
+  if (event.target.classList === "active") {
     event.target.classList.remove("active");
   }
   showingSubMenu = false;
   subMenuEl.style.top = "0";
-  // Task 5.4:
+
+  //Task 5.4
   for (let element of topMenuLinks) {
-    console.log(element);
+    // console.log(element);
     element.classList.remove("active");
   }
 
-//Task 5.5
+  //Task 5.5:
+  // event.target.setAttribute('class','active')
+  //or
+  event.target.classList.add("active");
+  // console.log(topMenuLinks.classList)
 
-event.target.classList.add('active')
+  // Task 5.6:
+  console.log(event.target.children);
+  if (event.target.children) {
+    showingSubMenu = true;
+  } else {
+    showingSubMenu = false;
+  }
+  mainEl.innerHTML=`<h1>${event.target.text}</h1>`
+  //Task 5.7
+  if (showingSubMenu) {
+    // console.log(event.target.subLinks)
+    if(clickedItem.subLinks) {
+      buildSubMenu(clickedItem.subLinks);
+    }    
+    subMenuEl.style.top = "100%";
+  } else {
+    subMenuEl.style.top = "0";
+  }
 
+  //Task 5.8:code buildSubMenu:
+  function buildSubMenu(sublinks) {
+    subMenuEl.textContent = "";
+    for (let subEle of sublinks) {
+      const subA = document.createElement("a");
+      subA.setAttribute("href", subEle.href);
+      // console.log(subEle.text)
+      subA.text = subEle.text;
+      // console.log(subA);
+      subMenuEl.append(subA);
+    }
+  }
+  //Task 6.0
+  subMenuEl.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (e.target.tagName !== "A") {
+      return;
+    }
+    // console.log(e.target);
+    //Task 6.1
+    showingSubMenu = false;
+    subMenuEl.style.top = "0";
 
-//Task 5.6
-// console.log(event.target.children)
-if(event.target.children){
-  showingSubMenu=true
-}else{
-  showingSubMenu=false
-}
-
-
-  return;
+    //Task 6.2
+    for (let element of topMenuLinks) {
+      element.classList.remove("active");
+    }
+    //Task 6.3:
+    mainEl.innerHTML = `<h1>${e.target.text}</h1>`;
+  });
 });
